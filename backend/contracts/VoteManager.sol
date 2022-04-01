6428d77916d396f7fdf11dd0914104e4100624f6
arthursiq5 <arthur.siq5@gmail.com>
2022-03-31 21:20:16 -0300

    Iniciada estrutura da struct

---
+++ backend/contracts/VoteManager.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense
+//specific solidity cersion
+pragma solidity ^0.8.7;
+
+struct Candidate {
+    uint id;
+    uint totalVote;
+    string name;
+    string imageHash;
+    address candidateAddress;
+}
+
